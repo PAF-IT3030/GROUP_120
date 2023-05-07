1b5b95e66c2906aace3551ac4d5decd8830af567
malinijayaweera <116679562+malinijayaweera@users.noreply.github.com>
2023-05-07 22:14:54 +0530

    models
    models for private chat

---
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/model/MessageModel.java
@@ -0,0 +1,72 @@
+package com.javamaster.messagingapp.model;
+
+import java.util.Date;
+public class MessageModel {
+
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private Date date;
+    private StatusModel status;
+
+    public MessageModel() {}
+
+    public MessageModel(String senderName, String receiverName, String message, Date date, StatusModel status) {
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.message = message;
+        this.date = date;
+        this.status = status;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public StatusModel getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusModel status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "senderName='" + senderName + '\'' +
+                ", receiverName='" + receiverName + '\'' +
+                ", message='" + message + '\'' +
+                ", date=" + date +
+                ", status=" + status +
+                '}';
+    }
+}
