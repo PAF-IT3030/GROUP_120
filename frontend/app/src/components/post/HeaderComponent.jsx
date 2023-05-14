caf05eac48b706164a3f1ac0e058a7a4fadc7888
srithanujan <srithanujan@gmail.com>
2023-05-15 03:35:31 +0530

    ui-update

--- frontend/app/src/components/post/HeaderComponent.jsx
+++ frontend/app/src/components/post/HeaderComponent.jsx
@@ -31,7 +31,7 @@ class HeaderComponent extends Component {
                     <OverlayTrigger placement={"bottom"} overlay={<Tooltip id={"tooltip-bottom"}>Back to home</Tooltip>}><Link className="nav-link" to="/welcome/"><img src={Logo} alt="Logo" className="rmitLogo"/></Link>
                     </OverlayTrigger>
                     <ul className="navbar-nav d-md-block d-none" style={{marginRight: "1.1rem"}}>
-                        <Link className="nav-link" to="/welcome/">Home</Link>
+                        <Link className="nav-link" to="/welcome/">Mighty Foods</Link>
                     </ul>
 
                     <SearchBarComponent refreshInfo={this.props.refreshInfo}></SearchBarComponent>
@@ -43,7 +43,7 @@ class HeaderComponent extends Component {
                             <OverlayTrigger key={"bottom"} placement={"bottom"} overlay={<Tooltip id={'tooltip-bottom'}> Visit your profile </Tooltip>}>
                                 <li className={"toProfile"}><Avatar username={username}/><a className="nav-link" href={'/profile/' + username}>{username}</a></li>
                             </OverlayTrigger>
-                            <Link className="nav-link d-md-none" to="/welcome/">Home</Link>
+                            <Link className="nav-link d-md-none" to="/welcome/">Mighty Foods</Link>
 
                             <li><Link className="nav-link" name="logout" to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>
                         </ul>
--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/post/Post.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/post/Post.java
@@ -9,118 +9,142 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Todo")
 public class Post {
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String username;
-	private String description;
-	private Date targetDate;
-	private boolean isDone;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String description;
+    private Date targetDate;
+    private boolean isDone;
 
-	@ElementCollection(fetch = FetchType.LAZY)
-	private List<PostComment> comments = new ArrayList<>();
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<PostComment> comments = new ArrayList<>();
 
-	public Post() {
+    public Post() {
 
-	}
+    }
 
-	public Post(long id, String username, String description, Date targetDate, boolean isDone) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.description = description;
-		this.targetDate = targetDate;
-		this.isDone = isDone;
-		this.comments = new ArrayList<PostComment>();
-	}
-	
-	public Post addComment(PostComment comment) {
-		// Check if a comment with the same ID already exists in the list
-		for (int i = 0; i < comments.size(); i++) {
-			if (comments.get(i).getId() == comment.getId()) {
-				// Update the existing comment with the new values
-				comments.set(i, comment);
-				return this;
-			}
-		}
-		// If a comment with the same ID does not exist in the list, add the new comment
-		this.comments.add(comment);
-		return this;
-	}
+    public Post(long id, String username, String description, Date targetDate, boolean isDone) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.isDone = isDone;
+        this.comments = new ArrayList<PostComment>();
+    }
+    
+    public Post addComment(PostComment comment) {
+        // Check if a comment with the same ID already exists in the list
+        for (int i = 0; i < comments.size(); i++) {
+            if (comments.get(i).getId() == comment.getId()) {
+                // Update the existing comment with the new values
+                comments.set(i, comment);
+                return this;
+            }
+        }
+        // If a comment with the same ID does not exist in the list, add the new comment
+        this.comments.add(comment);
+        return this;
+    }
 
-	public List<PostComment> getComments() {
-		return this.comments;
-	}
+    public List<PostComment> getComments() {
+        return this.comments;
+    }
 
-	public void setComments(List<PostComment> list) {
-		this.comments = list;
-	}
-	public Long getId() {
-		return id;
-	}
+    public void setComments(List<PostComment> list) {
+        this.comments = list;
+    }
+    
+    public Post addComments(List<PostComment> comments) {
+        // Loop through the new comments list and add each one to the existing comments list
+        for (PostComment comment : comments) {
+            // Check if a comment with the same ID already exists in the list
+            boolean commentExists = false;
+            for (int i = 0; i < this.comments.size(); i++) {
+                if (this.comments.get(i).getId() == comment.getId()) {
+                    // Update the existing comment with the new values
+                    this.comments.set(i, comment);
+                    commentExists = true;
+                    break;
+                }
+            }
+            // If a comment with the same ID does not exist in the list, add the new comment
+            if (!commentExists) {
+                this.comments.add(comment);
+            }
+        }
+        return this;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public Date getTargetDate() {
-		return targetDate;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setTargetDate(Date targetDate) {
-		this.targetDate = targetDate;
-	}
+    public Date getTargetDate() {
+        return targetDate;
+    }
 
-	public boolean isDone() {
-		return isDone;
-	}
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
 
-	public void setDone(boolean isDone) {
-		this.isDone = isDone;
-	}
+    public boolean isDone() {
+        return isDone;
+    }
 
-	public void printComments() {
-		System.out.println("Print comments");
-		System.out.println(Arrays.toString(comments.toArray()));
-		System.out.println("Print first comment");
-		System.out.println(comments.get(0).toString());
-	}
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
+    public void printComments() {
+        System.out.println("Print comments");
+        System.out.println(Arrays.toString(comments.toArray()));
+        System.out.println("Print first comment");
+        System.out.println(comments.get(0).toString());
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Post other = (Post) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Post other = (Post) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
 }
